4b84c1df0b4fc8881f027d0f46b8e8994969975c
servet0 <servetacar540@gmail.com>
2025-08-16 22:07:39 +0300

    Add test classes

--- src/test/java/com/yourcompany/iotplatform/iot_device_management/IotDeviceManagementApplicationTests.java
+++ src/test/java/com/yourcompany/iotplatform/iot_device_management/IotDeviceManagementApplicationTests.java
@@ -2,12 +2,18 @@ package com.yourcompany.iotplatform.iot_device_management;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+/**
+ * Ana uygulama test sınıfı
+ * Spring Boot context'inin yüklenip yüklenmediğini test eder
+ */
 @SpringBootTest
+@ActiveProfiles("test")
 class IotDeviceManagementApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+        // Spring Boot context'inin başarıyla yüklendiğini doğrular
+    }
 }
